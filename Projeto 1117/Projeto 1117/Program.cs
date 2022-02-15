cd0aaeae677f919ccc55e94b4e23b2c9d91786d3
karluze <karluzem@gmail.com>
2022-02-15 16:57:44 -0300

    Projeto 1117

---
+++ Projeto 1117/Projeto 1117/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota = 0, soma = 0;
+            int cont = 0;
+
+            while (cont < 2)
+            {
+                nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (nota >= 0 && nota <= 10)
+                {
+                    soma = soma + nota;
+
+                    cont = cont + 1;
+                }
+
+                else
+                {
+                    Console.WriteLine("nota invalida");
+                }
+
+
+            }
+
+            var media = soma / 2;
+            Console.WriteLine("media = " + media);
+
+
+            //Console.ReadLine();
+        }
+    }
+}
